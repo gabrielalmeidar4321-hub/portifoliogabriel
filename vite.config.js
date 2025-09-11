6598a92b30de5d16f6fdec3a8ffb0f25a0796459
michellemarcondes <mpinheiromarcondes@gmail.com>
2025-09-11 20:04:59 -0300

    agora vai plds

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfoliogabriel/',   // <- tem que ser esse valor
+  base: '/portifoliogabriel/',   // <- tem que ser esse valor
 })
